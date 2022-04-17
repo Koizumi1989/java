ce7d67c226d9490ba56c15d17d7bbb7b6c5001a3
Koizumi1989 <08033431635a@gmail.com>
2022-04-17 22:03:58 +0900

    10

---
+++ JavaJava/src/exception/Exception01.java
@@ -0,0 +1,10 @@
+package exception;
+
+public class Exception01 {
+	public static void main(String[] args) {
+		System.out.println("100 ÷ 0 ?");
+		int result = 100 / 0;
+		System.out.println("Calculation result" + result);
+		System.out.println("the end of the program");
+	}
+}
