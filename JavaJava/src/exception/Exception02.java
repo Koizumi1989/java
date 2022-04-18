6cdff3a308783389d9d5f973d953632a2a23aae0
Koizumi1989 <08033431635a@gmail.com>
2022-04-18 21:14:13 +0900

    10

--- JavaJava/src/exception/Exception02.java
+++ JavaJava/src/exception/Exception02.java
@@ -1,15 +1,21 @@
 package exception;
 
 public class Exception02 {
-	public static void main(String[] args) {
-		try {
-			System.out.println("100 ÷ 0 ?");
-			int result = 100 / 0;
-			System.out.println("Calculation result");
-		} catch (ArithmeticException e) {
-			System.out.println("Exception occurs");
-		} finally {
-			System.out.println("the end of the program");
-		}
-	}
+    public static void main(String[] args) {
+        // try-catch-finally文の開始
+        try {
+            // tryブロック：例外が発生する可能性がある処理を記述
+            System.out.println("100 ÷ 0 は？");
+            // Exception01と同じく、例外が発生
+            int result = 100 / 0;
+            System.out.println("計算結果" + result);
+        // ArithmeticExceptionクラスの例外が発生した場合の例外処理
+        } catch (ArithmeticException e) {
+            // 例外クラスが一致するので、例外処理が実行される
+            System.out.println("例外が発生");
+        } finally {
+            // 例外発生しても処理を実行
+            System.out.println("プログラム終了");
+        }
+    }
 }
