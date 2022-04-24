f582fb5cafccdc0ca49cf9e3eccae80f1783c8bf
Koizumi1989 <08033431635a@gmail.com>
2022-04-24 21:18:46 +0900

    explanation phone

--- JavaJava/src/extend/Interface01.java
+++ JavaJava/src/extend/Interface01.java
@@ -4,16 +4,21 @@ import extend.phone.Camera;
 import extend.phone.MobilePhone;
 
 public class Interface01 {
+    public static void main(String[] args) {
+        // MobilePhoneインスタンスを生成
+        MobilePhone mobilePhone = new MobilePhone("000-1111-2222");
 
-	public static void main(String[] args) {
-		MobilePhone mobilePhone = new MobilePhone("080-3343-1635");
-		
-		System.out.println("maximum phone number" + MobilePhone.MAX_NUMBER_DIGITS + "digit");
-		mobilePhone.call("080-3343-1635");
-		Interface01.classTakePicture(mobilePhone);
-	}
-	
-	public static void classTakePicture(Camera camera) {
-		camera.takePicture();
-	}
+        // Phoneインターフェースのフィールド（定数）を参照
+        System.out.println("電話番号は最大" + MobilePhone.MAX_NUMBER_DIGITS + "桁です。");
+        // Phoneインターフェースの抽象メソッドを実装したメソッドを呼び出し
+        mobilePhone.call("99-8888-7777");
+        // classTakePictureメソッドの呼び出し
+        Interface01.classTakePicture(mobilePhone);
+    }
+
+    // Cameraインターフェース実装したクラスは呼び出しできる
+    public static void classTakePicture(Camera camera) {
+        // アップキャストされた変数のメソッドを呼び出し
+        camera.takePicture();
+    }
 }
